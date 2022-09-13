6e4c0cdb7cd6d3c12c70095daed1e4b9f708de5f
tataki26 <tataki26@gmail.com>
2022-09-13 09:32:55 +0900

    Create variable.js
    - to learn JavaScript variable

---
+++ variable.js
@@ -0,0 +1,19 @@
+// 이미 정의된 변수
+let name = "Mike";
+age = 30;
+
+// 알림창
+// alert(name);
+// 콘솔 출력
+// console.log(age);
+
+// 앞서 정의된 변수이므로 에러
+// let name = "google";
+// let 제외하고 선언하면 재정의 가능
+name = "google";
+
+// 절대로 바뀌지 않는 상수(대문자로 정의)
+const PI = 3.14;
+
+// TIP
+// 우선, const로 정의 후 바뀔 여지가 있는 변수만 let으로 선언
